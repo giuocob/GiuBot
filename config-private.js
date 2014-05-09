9218c84e7fbece8337d9670d96f3d1be53b2cfe1
giuocob <pentameterpete@gmail.com>
2014-05-08 20:56:18 -0400

    added private config

---
+++ config-private.js
@@ -0,0 +1,11 @@
+module.exports = {
+	'prod': {
+		server: 'irc2.speedrunslive.com',
+		nick: 'GiuBot',
+		username: 'GiuBot',
+		password: 'pentameterpete',
+		owner: 'giuocob',
+		racebot: 'RaceBot',
+		homeChannel: '#speedrunslive'
+	}
+}
